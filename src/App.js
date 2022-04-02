3939ee07640a3a82a539859dc358ea7670ca0e71
sahatreact <sahatreactjs@gmail.com>
2022-04-03 00:03:08 +0700

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import "./App.css";
+import { HomePage } from "./components/Home.page";
+import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
+import { SuperHeroesPage } from "./components/SuperHeroes.page";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/super-heroes">Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+          <Route path="/super-heroes" element={<SuperHeroesPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
