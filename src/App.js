78ecc69c07a2d33b3338f8ac835ce3148ac4f8a9
sahatreact <sahatreactjs@gmail.com>
2022-04-03 14:11:20 +0700

    fetching data with useQuery

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 import "./App.css";
 import { HomePage } from "./components/Home.page";
 import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
 import { SuperHeroesPage } from "./components/SuperHeroes.page";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/super-heroes">Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
-          <Route path="/super-heroes" element={<SuperHeroesPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/super-heroes">Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/rq-super-heroes" component={RQSuperHeroesPage} />
+            <Route path="/super-heroes" component={SuperHeroesPage} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
