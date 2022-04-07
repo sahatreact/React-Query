bd65ed69b0e15256a99779afdc1ebdf2ef7a3c07
sahatreact <sahatreactjs@gmail.com>
2022-04-08 01:51:55 +0700

    Success and Error Callback

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -6,11 +6,19 @@ const fetchSuperHeroes = () => {
 };
 
 export const RQSuperHeroesPage = () => {
+  const onSuccess = (data) => {
+    console.log("Perform side effect after data fetching", data);
+  };
+  const onError = (error) => {
+    console.log("Perform side effect after encountering error", error);
+  };
+
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-heroes",
     fetchSuperHeroes,
     {
-      enabled: false,
+      onSuccess: onSuccess,
+      onError: onError,
     }
   );
 
